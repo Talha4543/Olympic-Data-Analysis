0f7005cd5ea37ffbbb2e18d9336d5d98613051af
Muhammad Talha <148857043+Talha4543@users.noreply.github.com>
2025-08-19 14:21:14 +0500

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-df = pd.read_csv("https://raw.githubusercontent.com/Talha4543/Olympic-Data-Analysis/main/athlete_events.csv")
+df = pd.read_csv('athlete_events.csv')
+
 region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessor.preprocess(df,region_df)
